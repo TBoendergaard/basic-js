045b24db54b80eea868c26db05185e1e290b09ff
Vladimir <kyrytin@gmail.com>
2022-04-28 22:00:50 +0200

    feat: transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,74 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  let transformedArrayData = new Array(arr.length).fill(1);
+
+  const arrayHasIndex = (array, index) => {
+    return !(array?.[index] === undefined);
+  }
+
+  const discardNext = (currentIndex) => {
+    transformedArrayData[currentIndex] = 0;
+
+    if (arrayHasIndex(transformedArrayData, currentIndex + 1)) {
+      transformedArrayData[currentIndex + 1]--;
+    }
+  }
+
+  const discardPrev = (currentIndex) => {
+    transformedArrayData[currentIndex] = 0;
+
+    if (arrayHasIndex(transformedArrayData, currentIndex - 1)) {
+      
+      transformedArrayData[currentIndex - 1]--;
+    }
+  }
+
+  const doubleNext = (currentIndex) => {
+    transformedArrayData[currentIndex] = 0;
+    
+    if (arrayHasIndex(transformedArrayData, currentIndex + 1)) {
+      transformedArrayData[currentIndex + 1] += transformedArrayData[currentIndex + 1] > 0 ? 1 : 0;
+    }
+  }
+
+  const doublePrev = (currentIndex) => {
+    transformedArrayData[currentIndex] = 0;
+
+    if (arrayHasIndex(transformedArrayData, currentIndex - 1)) {
+      transformedArrayData[currentIndex - 1] += transformedArrayData[currentIndex - 1] > 0 ? 1 : 0;
+    }
+  }
+
+  const CONTROLS = {
+    '--discard-next': discardNext,
+    '--discard-prev': discardPrev,
+    '--double-next': doubleNext,
+    '--double-prev': doublePrev,
+  }
+
+  let transfromedArray = [];
+  
+  arr.forEach((item, index) => {
+    if (isNaN(+item) && CONTROLS.hasOwnProperty(item)) {
+      CONTROLS[item](index);
+    }
+  })
+  
+  transformedArrayData.forEach((item, index) => {
+    for(let i = 0; i < item; i++) {
+      transfromedArray.push(arr[index]);
+    }
+  })
+
+  return transfromedArray;
 }
+
 
 module.exports = {
   transform
