d1494c6482d3c7c284a476d90364472572f07779
Vladimir <kyrytin@gmail.com>
2022-04-30 11:50:41 +0200

    feat: vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,65 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class VigenereCipheringMachine {
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    this.square = new Array();
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  generateSquare() {
+    for (let i = 0; i < this.alphabet.length; i++) {
+      this.square[i] = this.alphabet.slice(i).concat(this.alphabet.slice(0, i));
+    }
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    this.generateSquare();
+    message = message.toUpperCase().split('');
+    key = key.toUpperCase().split('');
+
+    let result = new Array();
+    let keyIndex = 0;
+
+    result = message.map(char => {
+      if (!this.alphabet.includes(char)) {
+        return char;
+      }
+
+      const row = this.alphabet.indexOf(char);
+      const col = this.alphabet.indexOf(key[keyIndex % key.length]);
+      keyIndex++;
+      return this.square[row][col];
+    })
+
+    return this.isDirect ? result.join('') : result.reverse().join('');
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    this.generateSquare();
+    message = message.toUpperCase().split('');
+    key = key.toUpperCase().split('');
+
+    let result = new Array();
+    let keyIndex = 0;
+
+    result = message.map(char => {
+      if (!this.alphabet.includes(char)) {
+        return char;
+      }
+
+      const row = this.alphabet.indexOf(key[keyIndex % key.length]);
+      const col = this.square[row].indexOf(char);
+      keyIndex++;
+      return this.alphabet[col];
+    })
+
+    return this.isDirect ? result.join('') : result.reverse().join('');
   }
 }
 
